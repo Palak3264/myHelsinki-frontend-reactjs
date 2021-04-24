2ad389d68e819b733114b1d1420a5544d2479950
palakarora2417 <palakarora2417@gmail.com>
2021-04-25 01:07:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import Navibar from './components/Navibar';
+import Places from './components/Places';
 import './App.css';
+import Footor from "./components/Footor";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        document.title = "MyHelsinki";
+    });
+
+    return (
+        <Router>
+            <Navibar/>
+            <Places/>
+            <Footor/>
+        </Router>
+    );
 }
 
 export default App;
